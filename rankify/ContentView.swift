3b97157f905902bb4d9a5921ce76ecfece4573eb
vaasvi <vaasvich@usc.edu>
2025-04-28 13:11:04 -0700

    Initial Commit

---
+++ rankify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rankify
+//
+//  Created by Vaasvi Choudhary on 4/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rankify/rankifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rankifyApp.swift
+//  rankify
+//
+//  Created by Vaasvi Choudhary on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct rankifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
